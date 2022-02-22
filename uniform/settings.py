8634c0939c84506d31c0c470b1a39f707a7c27b1
gioitube <gioitube2k2@gmail.com>
2022-02-22 10:56:59 +0700

    first commit

--- uniform/settings.py
+++ uniform/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9q_crvmo5no@6)cjvjy$&0xd2v7t(cq#9%pu-3gvt3lzcqknrr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
